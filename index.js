2409a0a1950f14999730d2297dd085768ee8e9ab
Cauã Amorim <amorimcaua123@gmail.com>
2026-09-10 16:20:46 -0300

    testextx

---
+++ index.js
@@ -0,0 +1,5 @@
+document.getElementById("calcular") .addEventListener("click", function() {
+let peso = document.getElementById("peso").value;
+let altura = document.getElementById("altura").value;
+let medidas = document.getElementById("medidas corporais").value;
+}
