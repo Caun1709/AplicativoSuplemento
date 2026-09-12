4de264ced2e4eb655245e50c7575d6cfc71914b7
Cauã Amorim <amorimcaua123@gmail.com>
2026-09-12 20:38:40 -0300

    testextx2

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ let peso = document.getElementById("peso").value;
 let altura = document.getElementById("altura").value;
 let medidas = document.getElementById("medidas corporais").value;
 
-}
+let imc = peso / (altura/100)**2;
+})
