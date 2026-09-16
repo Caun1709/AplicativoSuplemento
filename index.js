8d628bb884af137b1ccca912f1550e9d0284ee91
Cauã Amorim <amorimcaua123@gmail.com>
2026-09-16 15:16:32 -0300

    testx4

--- index.js
+++ index.js
@@ -5,3 +5,24 @@ let medidas = document.getElementById("medidas corporais").value;
 
 let imc = peso / (altura/100)**2;
 })
+
+
+
+function calcularGordura(peso, altura, idade, genero) {
+    const imc = peso / (altura * altura);
+
+    let percentual;
+
+    if (genero === "masculino") {
+        percentual = (1.20 * imc) + (0.23 * idade) - 16.2;
+    } else {
+        percentual = (1.20 * imc) + (0.23 * idade) - 5.4;
+    }
+    
+
+    return percentual.toFixed(2);
+}
+
+const resultado = calcularGordura(70, 1.75, 20, "masculino");
+
+console.log("Percentual estimado de gordura: " + resultado + "%");
